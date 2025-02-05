b329fa110fcd537451bf6b74188bd62525d5c19c
aidenl93 <aidenl93@nycstudents.net>
2025-02-05 10:04:02 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,41 @@
+"""? # Strings are for representing characters, names, words, etc
+name = "David"
+# integer represents whole numbers
+age: 14
+#floats represent decimals
+wallet = 5.45 
+#boolean represents true/false, used in evaluations
+graduated = False
+
+def add(x,y):
+    print(x+y)
+#input asks the user a questions and stores response as value 
+# bill = float(input("what was the bill?"))
+# print(type(bill))
+# add(40, bill)
+
+#lists""" 
+""" students = ["Joanna", "Deivid", "David", "Other David", "Ethan"]
+#similar to saying for i in range(5): print(students[i])
+print(students[4])
+for student in students:
+    print(student)
+moneys = [1,2,3,4,5,6]
+for money in moneys:
+    print(money)
+total = 0 """
+""" x = "thingy"
+y = x.split()
+z = y[0]
+print(y)
+print(z)""" 
+# make function for sentence
+# count words for user
+# state number of words
+def sentence(input):
+    print(input)
+sentence("Please make a sentence.")
+
+
+
+
