1dd7207014819755b9bad4317f2c513975f5b3e6
aidenl93 <aidenl93@nycstudents.net>
2025-02-07 09:54:39 -0500

    Update lesson of control.py

--- lesson of control.py
+++ lesson of control.py
@@ -1,7 +1,28 @@
-def login(password):
+""" def login(password):
     if password == "secret":
         print("logged in")
     else:
         print("incorrect")
 x = input("whats password")
-login(x)
+login(x) """
+
+"""def grade(score):
+    if score >= 92:
+        print("A")
+    elif score >= 82:
+        print("B")
+    elif score >= 72:
+        print("C")
+    else:
+        print("F")
+x = int(input("Whats score"))
+grade(x)"""
+
+def gamble(age,id):
+    if age >= 21:
+        if id: 
+            print("gamble")
+        else:
+            print("show id")
+    else:
+        print("too young")
