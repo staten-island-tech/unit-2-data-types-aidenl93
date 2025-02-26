7aba4e708628a76855bc4364c132cf4ebc8b2078
aidenl93 <aidenl93@nycstudents.net>
2025-02-26 09:43:44 -0500

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,5 @@
 
 def skins3(money, cost, available):
-    while money > cost or available == False:
+    while money > cost or available == False or money > cost and available == False:
         print("no money")
     print(f"current balance: {money}. \n current cost: {cost} go buy")
